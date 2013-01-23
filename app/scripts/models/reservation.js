22f0e219ec6d3911a9b7bd301865b4a75b240f24
John Brunton <john_brunton@hotmail.co.uk>
2013-01-23 13:08:00 +0000

    skeleton model file

---
+++ app/scripts/models/reservation.js
@@ -0,0 +1,18 @@
+Reservation = function(data) {
+  // this.start = ...
+  // this.end = ...
+  // this.location = ...
+}
+
+Reservation.fetch = function(id) {
+  // ...
+}
+
+Reservation.fetch_all = function() {
+  // ...
+}
+
+// refresh with data from server
+Reservation.prototype.refresh = function() {
+  
+}
